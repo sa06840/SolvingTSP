d82c96674c5e965c8f077ab6e4759abcf3d25d5d
Sajeel Alam <sa06840@st.habib.edu.pk>
2023-12-10 14:12:11 +0500

    implemented aco and pso

---
+++ particleSwarmOptimization/tsppso/__init__.py
@@ -0,0 +1,148 @@
+from pso import TSPPSO
+from tsp import Problem
+from datatypes import Position
+
+
+def berlin52():
+    from datasets import TSPDataset
+    dataset = TSPDataset()
+    dataset.read('./particleSwarmOptimization/datasets/berlin52.tsp')
+    dataset.unique(eps=1e-4, inplace=True)
+    print('dataset size:', len(dataset.positions))
+
+    import random
+    seed = random.randint(0, 999999)
+    rng = random.Random(seed)
+    print('seed used:', seed)
+    # 7544.365901904087 - 328015
+
+    problem = Problem(xy=dataset.positions)
+    optimizer = TSPPSO(100, 100, p1=0.95, p2=0.03, p3=0.02, max_no_improv=3, rng=rng)
+    solution = optimizer.minimize(problem)
+
+    print()
+    print('seq:\n{}\n\ncost:\n{}'.format(solution.sequence, solution.cost))
+
+
+def zi929():
+    from datasets import TSPDataset
+    dataset = TSPDataset()
+    dataset.read('./particleSwarmOptimization/datasets/zi929.tsp')
+    dataset.unique(eps=1e-4, inplace=True)
+    print('dataset size:', len(dataset.positions))
+
+    import random
+    rng = random.Random()
+
+    problem = Problem(xy=dataset.positions)
+    # optimizer = TSPPSO(1000, 20, max_no_improv1=1000, max_no_improv2=5, rng=rng)
+    optimizer = TSPPSO(100, 100, p1=0.95, p2=0.03, p3=0.02, max_no_improv=3, rng=rng)
+    solution = optimizer.minimize(problem)
+
+    print()
+    print('seq:\n{}\n\ncost:\n{}'.format(solution.sequence, solution.cost))
+
+
+def lu634():
+    from datasets import TSPDataset
+    dataset = TSPDataset()
+    dataset.read('./particleSwarmOptimization/datasets/lu634.tsp')
+    dataset.unique(eps=1e-4, inplace=True)
+    print('dataset size:', len(dataset.positions))
+
+    import random
+    rng = random.Random(97855)
+
+    problem = Problem(xy=dataset.positions)
+    optimizer = TSPPSO(1000, 40, max_no_improv1=20, max_no_improv2=5, rng=rng)
+    solution = optimizer.minimize(problem)
+
+    print()
+    print('seq:\n{}\n\ncost:\n{}'.format(solution.sequence, solution.cost))
+
+
+def canada():
+    from datasets import TSPDataset
+    dataset = TSPDataset()
+    dataset.read('./particleSwarmOptimization/datasets/canada.tsp')
+    dataset.unique(inplace=True)
+    print('dataset size:', len(dataset.positions))
+
+    import random
+    rng = random.Random(97855)
+
+    problem = Problem(xy=dataset.positions)
+    optimizer = TSPPSO(200, 20, max_no_improv1=10, rng=rng)
+    solution = optimizer.minimize(problem)
+
+    print()
+    print('seq:\n{}\n\ncost:\n{}'.format(solution.sequence, solution.cost))
+
+
+def qa194():
+    from datasets import TSPDataset
+    dataset = TSPDataset()
+    dataset.read('./particleSwarmOptimization/datasets/qa194.tsp')
+    dataset.unique(eps=1e-4, inplace=True)
+    print('dataset size:', len(dataset.positions))
+
+    import random
+    seed = random.randint(0, 999999)
+    rng = random.Random(seed)
+    print('seed used:', seed)
+    # 7544.365901904087 - 328015
+
+    problem = Problem(xy=dataset.positions)
+    optimizer = TSPPSO(200, 100, p1=0.95, p2=0.03, p3=0.02, max_no_improv=3, rng=rng)
+    solution = optimizer.minimize(problem)
+
+    print()
+    print('seq:\n{}\n\ncost:\n{}'.format(solution.sequence, solution.cost))
+
+
+def test():
+    def brute_force_tsp(problem: Problem):
+        import sys
+        from itertools import permutations
+        from tsppso.tsp import evaluate_cost
+        min_seq = None
+        min_cost = sys.float_info.max
+        base_seq = list(range(len(problem.distances)))
+        for seq in permutations(base_seq):
+            seq = list(seq)
+            cost = evaluate_cost(seq, problem.distances)
+            if cost <= min_cost:
+                min_cost = cost
+                min_seq = seq
+        return min_seq, min_cost
+
+    positions = [
+        (2, 9),  # A
+        (8, 9),  # B
+        (3, 7),  # C
+        (1, 6),  # D
+        (7, 6),  # E
+        (4, 4),  # F
+        (7, 1),  # G
+    ]
+
+    problem = Problem(xy=positions)
+
+    seq, cost = brute_force_tsp(problem)
+    print("seq:", list(seq), "cost:", cost)
+
+    import random
+    seed = random.randint(0, 100000)
+    print("seed:", seed)
+
+    optimizer = TSPPSO(100, 30, max_no_improv1=10, rng=random.Random(seed))
+    solution = optimizer.minimize(problem)
+
+
+if __name__ == "__main__":
+    # test()
+    # canada()
+    # lu634()
+    # zi929()
+    # berlin52()
+    qa194()
---
+++ particleSwarmOptimization/tsppso/pso.py
@@ -0,0 +1,27 @@
+import abc
+from dataclasses import dataclass
+from random import Random
+
+from tsp import Problem, Solution, pso_minimize
+
+
+class PSO(abc.ABC):
+    @abc.abstractmethod
+    def minimize(self, problem: Problem) -> Solution:
+        pass
+
+
+@dataclass
+class TSPPSO(PSO):
+    n: int
+    poolsize: int
+    p1: float = 0.9
+    p2: float = 0.05
+    p3: float = 0.05
+    max_no_improv: int = 3
+    rng: Random = None
+
+    def minimize(self, problem: Problem) -> Solution:
+        return pso_minimize(
+            self.n, self.poolsize, problem.distances, self.p1, 
+            self.p2, self.p3, self.max_no_improv, self.rng)
